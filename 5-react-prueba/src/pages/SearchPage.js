27f7f12a0538778841d0a90b9c320a15b30eb256
jc <jcsanchez@coppel.com>
2022-10-27 19:27:20 -0600

    Feat: Se sube avance de clase (27-OCT)

---
+++ 5-react-prueba/src/pages/SearchPage.js
@@ -0,0 +1,13 @@
+import Card from "../components/Card";
+import SearchFilters from "../components/SearchFilters";
+
+function SearchPage() {
+    return(
+        <>
+            <SearchFilters></SearchFilters>
+            <Card/>
+        </>
+    );
+}
+
+export default SearchPage;
